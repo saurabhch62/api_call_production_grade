a205edf34c994d8f6b918e3a3c78e0df2f63335e
Saurabh Chaurasia <about.srb@gmail.com>
2026-07-06 19:11:52 +0530

    added offset pagination, change write mode to append as required for yield

--- api_call.py
+++ api_call.py
@@ -7,20 +7,23 @@ class ApiExtractor:
         self.base_url = base_url
         self.api_token = api_token
         self.page_size = page_size
-        self.session = requests.session()
+        self.session = requests.session() # added session to reuse the connection for multiple API calls
 
         self.session.headers.update({
-                "Authorization" : api_token,
-                "Accept": "application/json"
-        }
-        )
-        self.session.timeout = 30
+                                    "Authorization" : api_token,
+                                    "Accept": "application/json"
+                                    })
+                                    
+        self.session.timeout = 30 # added timeout for sessions to avoid hanging of the request in case of network issues
 
     def ExtractAll(self,endpoint, param):
 
+        if param is None:
+            param = {"limit": 5, "offset": 0}
+
         url = self.base_url + endpoint
-        response = self.session.request("get",url)
-        data = response.json()        
+        response = self.session.request("get",url,params=param)
+        data = response.json()
         for record in data:            
             yield record
 
@@ -33,7 +36,11 @@ extractor = ApiExtractor(
         page_size = 200
 )
 
+#define limit and offset for offset - pagination
+limit = 100
+offset = 0
 
-with open("output/output_data.json",'w') as f:
-    for record in extractor.ExtractAll(endpoint="posts", param ={}):
+with open("output/output_data.json",'a') as f:
+    for record in extractor.ExtractAll(endpoint="posts", param={"limit": limit, "offset": offset}):
         f.write(json.dumps(record) + "\n")
+        offset += limit
